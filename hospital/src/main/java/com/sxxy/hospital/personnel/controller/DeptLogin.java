4a47bc2893117a1822dbe4e026f35257910ffdbe
chenlei <1575422375@qq.com>
2020-09-11 20:07:58 +0800

    部门登录

--- hospital/src/main/java/com/sxxy/hospital/personnel/controller/DeptLogin.java
+++ hospital/src/main/java/com/sxxy/hospital/personnel/controller/DeptLogin.java
@@ -1,4 +1,26 @@
 package com.sxxy.hospital.personnel.controller;
 
+
+import com.sxxy.hospital.personnel.service.DeptService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/deptLogin")
 public class DeptLogin {
+    //注入接口
+    @Autowired
+    DeptService deptService;
+
+    //部门登录
+    @RequestMapping("/login")
+    public String deptLogin(String deptNum, String deptPassword){
+        if(deptService.deptLodin(deptNum,deptPassword)!=null) {
+            return "personnel/dept/deptIndex";
+        }else {
+            return "personnel/login";
+        }
+    }
+
 }
--- hospital/src/main/java/com/sxxy/hospital/personnel/service/DoctorService.java
+++
@@ -1,9 +0,0 @@
-package com.sxxy.hospital.personnel.service;
-
-import com.sxxy.hospital.personnel.entity.Doctor;
-import org.springframework.data.repository.query.Param;
-
-public interface DoctorService {
-    //医生登录
-    Doctor doctorLodin(String doctorNum, String doctorPassword );
-}
