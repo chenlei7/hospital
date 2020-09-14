3f9abf9d0c857539941b28c55859f8c99b7382d7
LIUXIAOBO-SHUAI\Administrator <lxb>
2020-09-14 10:31:12 +0800

    病人实体类

--- hospital/src/main/java/com/sxxy/hospital/patient/entity/Patient.java
+++ hospital/src/main/java/com/sxxy/hospital/patient/entity/Patient.java
@@ -1,4 +1,163 @@
 package com.sxxy.hospital.patient.entity;
 
+
+
+import org.springframework.stereotype.Component;
+//病人信息实体类
+import javax.persistence.*;
+
+@Entity
+@Table(name = "patient_info")
 public class Patient {
+    //id，自增
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    Integer id;
+    //病人编号
+    @Column(name = "patient_num")
+    String patientNum;
+    //名字
+    @Column(name = "patient_name")
+    String patient_name;
+    //年龄
+    @Column(name = "patient_age")
+    Integer patientAge;
+    //地址
+    @Column(name = "patient_address")
+    String patientAddress;
+   //来看病日期
+    @Column(name = "patient_comeDate")
+    String patientComeDate;
+    //离开时间
+    @Column(name = "patient_leaveDate")
+    String patientLeaveDate;
+    //病情编号
+    @Column(name = "patient_illness_num")
+    String patientIllnessNum;
+    //主治医师编号
+    @Column(name = "patient_doctor_num")
+    String patientDoctorNum;
+    //账单号
+    @Column(name = "patient_bill_num")
+    String patientBillNum;
+    //护士编号
+    @Column(name = "patient_nurse_num")
+    String patientNurseNum;
+    //科室
+    @Column(name = "patient_room_num")
+    String patientRoomNum;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPatientNum() {
+        return patientNum;
+    }
+
+    public void setPatientNum(String patientNum) {
+        this.patientNum = patientNum;
+    }
+
+    public String getPatient_name() {
+        return patient_name;
+    }
+
+    public void setPatient_name(String patient_name) {
+        this.patient_name = patient_name;
+    }
+
+    public Integer getPatientAge() {
+        return patientAge;
+    }
+
+    public void setPatientAge(Integer patientAge) {
+        this.patientAge = patientAge;
+    }
+
+    public String getPatientAddress() {
+        return patientAddress;
+    }
+
+    public void setPatientAddress(String patientAddress) {
+        this.patientAddress = patientAddress;
+    }
+
+    public String getPatientComeDate() {
+        return patientComeDate;
+    }
+
+    public void setPatientComeDate(String patientComeDate) {
+        this.patientComeDate = patientComeDate;
+    }
+
+    public String getPatientLeaveDate() {
+        return patientLeaveDate;
+    }
+
+    public void setPatientLeaveDate(String patientLeaveDate) {
+        this.patientLeaveDate = patientLeaveDate;
+    }
+
+    public String getPatientIllnessNum() {
+        return patientIllnessNum;
+    }
+
+    public void setPatientIllnessNum(String patientIllnessNum) {
+        this.patientIllnessNum = patientIllnessNum;
+    }
+
+    public String getPatientDoctorNum() {
+        return patientDoctorNum;
+    }
+
+    public void setPatientDoctorNum(String patientDoctorNum) {
+        this.patientDoctorNum = patientDoctorNum;
+    }
+
+    public String getPatientBillNum() {
+        return patientBillNum;
+    }
+
+    public void setPatientBillNum(String patientBillNum) {
+        this.patientBillNum = patientBillNum;
+    }
+
+    public String getPatientNurseNum() {
+        return patientNurseNum;
+    }
+
+    public void setPatientNurseNum(String patientNurseNum) {
+        this.patientNurseNum = patientNurseNum;
+    }
+
+    public String getPatientRoomNum() {
+        return patientRoomNum;
+    }
+
+    public void setPatientRoomNum(String patientRoomNum) {
+        this.patientRoomNum = patientRoomNum;
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", patientNum='" + patientNum + '\'' +
+                ", patient_name='" + patient_name + '\'' +
+                ", patientAge=" + patientAge +
+                ", patientAddress='" + patientAddress + '\'' +
+                ", patientComeDate='" + patientComeDate + '\'' +
+                ", patientLeaveDate='" + patientLeaveDate + '\'' +
+                ", patientIllnessNum='" + patientIllnessNum + '\'' +
+                ", patientDoctorNum='" + patientDoctorNum + '\'' +
+                ", patientBillNum='" + patientBillNum + '\'' +
+                ", patientNurseNum='" + patientNurseNum + '\'' +
+                ", patientRoomNum='" + patientRoomNum + '\'' +
+                '}';
+    }
 }
