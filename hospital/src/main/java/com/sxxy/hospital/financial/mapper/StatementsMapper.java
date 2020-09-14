19dbca41d227d501c1f41fb1982fcc49443dafeb
tq <846678875@qq.com>
2020-09-14 17:05:56 +0800

    对财务的事务进行了添加（查询收入、支出、类型、全部）

--- hospital/src/main/java/com/sxxy/hospital/financial/mapper/StatementsMapper.java
+++ hospital/src/main/java/com/sxxy/hospital/financial/mapper/StatementsMapper.java
@@ -1,6 +1,7 @@
 package com.sxxy.hospital.financial.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
@@ -10,5 +11,12 @@ import java.util.Map;
 //@Component
 @Mapper
 public interface StatementsMapper {
+    //查询所有财务信息
     List<Map> findAll();
+    //用过类型查询财务信息
+    List<Map> findByType(@Param("type") String type);
+    //查询所有的支出信息
+    List<Map> findByPay();
+    //查询所有的收入信息
+    List<Map> findByRevenue();
 }
