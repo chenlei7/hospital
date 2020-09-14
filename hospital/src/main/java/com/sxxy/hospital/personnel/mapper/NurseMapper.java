8220023f5693bdd368aac756addf1c69f0f1124f
chenlei <1575422375@qq.com>
2020-09-14 11:08:27 +0800

    护士修改状态

--- hospital/src/main/java/com/sxxy/hospital/personnel/mapper/NurseMapper.java
+++ hospital/src/main/java/com/sxxy/hospital/personnel/mapper/NurseMapper.java
@@ -2,8 +2,23 @@ package com.sxxy.hospital.personnel.mapper;
 
 import com.sxxy.hospital.personnel.entity.Nurse;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface NurseMapper extends JpaRepository<Nurse,Object> {
+
+
+
+
+    //修改护士现状
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value = "update nurse_info set nurse_state=:nurseState  where  nurse_num in (:editNum) ")
+    int editNurseNowState(@Param("nurseState")String nurseState, @Param("editNum") List<String> editNum);
 }
--- hospital/src/main/java/com/sxxy/hospital/personnel/service/NurseService.java
+++ hospital/src/main/java/com/sxxy/hospital/personnel/service/NurseService.java
@@ -4,7 +4,14 @@ package com.sxxy.hospital.personnel.service;
 
 import com.sxxy.hospital.personnel.entity.Nurse;
 
+import java.util.List;
+
 public interface NurseService {
     //增加护士
     Nurse addNurse(Nurse nurse);
+
+
+
+    //修改护士现状
+    int editNurseNowState(String nurseState, List<String> editNum);
 }
