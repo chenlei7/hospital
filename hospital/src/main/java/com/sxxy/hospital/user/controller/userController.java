abad57a8962bdd717f016870af6d15a13bb34f6b
LIUXIAOBO-SHUAI\Administrator <lxb>
2020-09-15 12:51:39 +0800

    挂号换回来

--- hospital/src/main/java/com/sxxy/hospital/user/controller/userController.java
+++ hospital/src/main/java/com/sxxy/hospital/user/controller/userController.java
@@ -28,7 +28,7 @@ public class userController {
     //挂号界面
     @RequestMapping("/registered")
     public String goRegistered(){
-        return "patient/registered";
+        return "user/registered";
     }
 
 
