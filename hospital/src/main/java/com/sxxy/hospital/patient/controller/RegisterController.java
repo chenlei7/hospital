38e48e31b084d462d51fb0ddc3529756cf2f1783
LIUXIAOBO-SHUAI\Administrator <lxb>
2020-09-15 16:08:52 +0800

    曹阳java2

--- hospital/src/main/java/com/sxxy/hospital/patient/controller/RegisterController.java
+++ hospital/src/main/java/com/sxxy/hospital/patient/controller/RegisterController.java
@@ -1,5 +1,7 @@
 package com.sxxy.hospital.patient.controller;
 
+import com.sxxy.hospital.equipment.entity.Room;
+import com.sxxy.hospital.equipment.mapper.RoomMapper;
 import com.sxxy.hospital.patient.entity.Patient;
 import com.sxxy.hospital.patient.mapper.PatientMapper;
 import com.sxxy.hospital.personnel.entity.Doctor;
@@ -7,6 +9,7 @@ import com.sxxy.hospital.personnel.mapper.DoctorMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -21,6 +24,8 @@ public class RegisterController {
     PatientMapper patientMapper;
     @Autowired
     DoctorMapper doctorMapper;
+    @Autowired
+    RoomMapper roomMapper;
 
     //增加网上预约
     @PostMapping("/registerAdd")
@@ -50,9 +55,17 @@ public class RegisterController {
     @RequestMapping("/registered")
     public String goRegistered(Model model){
         List<Doctor> doctors = new ArrayList<>();
+        List<Room> rooms = new ArrayList<>();
+        rooms = roomMapper.findAll();
         doctors = doctorMapper.findAll();
         model.addAttribute("doctors",doctors);
+        model.addAttribute("rooms",rooms);
         return "patient/register";
+    }
+
+    @GetMapping("/findDoctor")
+    public String findDoctor(){
+        return null;
     }
 
 }
