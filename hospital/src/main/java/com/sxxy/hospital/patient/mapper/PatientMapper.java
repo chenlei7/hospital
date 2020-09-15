fc9023ba75afcba3753de02dcc19a5d5d1a40be6
LIUXIAOBO-SHUAI\Administrator <lxb>
2020-09-15 11:35:03 +0800

    修改病人

--- hospital/src/main/java/com/sxxy/hospital/patient/mapper/PatientMapper.java
+++ hospital/src/main/java/com/sxxy/hospital/patient/mapper/PatientMapper.java
@@ -2,9 +2,18 @@ package com.sxxy.hospital.patient.mapper;
 
 import com.sxxy.hospital.patient.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface PatientMapper extends JpaRepository<Patient,Integer> {
+
+    @Transactional()
+    @Modifying
+    @Query(nativeQuery = true,value = "update patient_info  set patient_leaveDate=:patientLeaveDate,patient_illness_num=:patientIllnessNum,patient_nurse_num=:patientNurseNum where patient_num =:patientNum")
+    int PatientUpdate(@Param("patientLeaveDate")String patientLeaveDate,@Param("patientIllnessNum")String patientIllnessNum,@Param("patientNurseNum")String patientNurseNum,@Param("patientNum")String patientNum);
 
 }
--- hospital/src/main/java/com/sxxy/hospital/patient/service/impl/PatientServiceImpl.java
+++ hospital/src/main/java/com/sxxy/hospital/patient/service/impl/PatientServiceImpl.java
@@ -1,8 +1,10 @@
 package com.sxxy.hospital.patient.service.impl;
 
+import com.sxxy.hospital.patient.entity.Patient;
 import com.sxxy.hospital.patient.mapper.PatientMapper;
 import com.sxxy.hospital.patient.service.PatientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,4 +13,9 @@ public class PatientServiceImpl implements PatientService {
     PatientMapper patientMapper;
 
 
+   //修改病人
+    @Override
+    public int PatientUpdate(Patient patient) {
+        return patientMapper.PatientUpdate(patient.getPatientLeaveDate(),patient.getPatientIllnessNum(),patient.getPatientNurseNum(),patient.getPatientNum());
+    }
 }
