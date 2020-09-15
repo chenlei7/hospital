0c1c7645550ef709d72de7dea6261ff6806afcce
LIUXIAOBO-SHUAI\Administrator <lxb>
2020-09-15 15:11:36 +0800

    曹阳java

--- hospital/src/main/java/com/sxxy/hospital/equipment/mapper/ConsumablesMapper.java
+++ hospital/src/main/java/com/sxxy/hospital/equipment/mapper/ConsumablesMapper.java
@@ -2,8 +2,19 @@ package com.sxxy.hospital.equipment.mapper;
 
 import com.sxxy.hospital.equipment.entity.Consumables;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface ConsumablesMapper extends JpaRepository<Consumables,Object> {
+    //采购药材
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true,value = "update consumables set consumables_number=:consumablesNumber+consumables_number  where  consumables_num in (:editNum) ")
+    int addConsumablesNumber(@Param("consumablesNumber") int consumablesNumber, @Param("editNum") List<String> editNum);
 }
--- hospital/src/main/java/com/sxxy/hospital/equipment/service/ConsumablesService.java
+++ hospital/src/main/java/com/sxxy/hospital/equipment/service/ConsumablesService.java
@@ -1,4 +1,13 @@
 package com.sxxy.hospital.equipment.service;
 
+import com.sxxy.hospital.equipment.entity.Consumables;
+
+import java.util.List;
+
 public interface ConsumablesService {
+    //增加药材
+    Consumables addConsumables(Consumables consumables);
+
+    //采购药材
+    int addConsumablesNumber(int consumablesNumber, List<String> editNum);
 }
