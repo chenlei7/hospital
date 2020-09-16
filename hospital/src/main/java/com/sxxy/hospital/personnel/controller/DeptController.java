f9d124b38b715acf23e524d39e3d68c04e948689
chenlei <1575422375@qq.com>
2020-09-16 11:06:18 +0800

    病人按日显示

--- hospital/src/main/java/com/sxxy/hospital/personnel/controller/DeptController.java
+++ hospital/src/main/java/com/sxxy/hospital/personnel/controller/DeptController.java
@@ -10,7 +10,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 @Controller
@@ -28,8 +31,32 @@ public class DeptController {
         if(deptService.deptLodin(deptNum,deptPassword)!=null) {
             List<Patient> patients = new ArrayList<>();
             patients = patientMapper.findAll();
-            System.out.println(patients);
             model.addAttribute("patients",patients);
+            int patientNoLeave = 0;
+            int nowPatientNum = 0;
+            int yesterdayNum = 0;
+            //今日
+            String nowDate = new SimpleDateFormat("yyyy-MM-dd").format(new Date()).toString();
+            //昨日
+            Calendar calendar = Calendar.getInstance();
+            calendar.add(Calendar.DATE, -1);
+
+            for (int i = 0; i < patients.size(); i++) {
+                if(patients.get(i).getPatientLeaveDate()!=null){
+                    patientNoLeave++;
+                }
+                if(patients.get(i).getPatientComeDate().equals(nowDate)){
+                    nowPatientNum++;
+                }
+                if(patients.get(i).getPatientComeDate().equals(new SimpleDateFormat("yyyy-MM-dd").format(calendar.getTime()))){
+                    yesterdayNum++;
+                }
+            }
+            System.out.println(yesterdayNum);
+            model.addAttribute("nowPatientNum",nowPatientNum);
+            model.addAttribute("yesterdayNum",yesterdayNum);
+            model.addAttribute("patientNoLeave",patientNoLeave);
+
             return "personnel/index";
         }else {
             return "personnel/login";
