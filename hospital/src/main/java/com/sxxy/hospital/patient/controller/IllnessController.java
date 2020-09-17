04f7f923851d28177197a16d7d196119f036ff3c
LIUXIAOBO-SHUAI\Administrator <lxb>
2020-09-17 14:13:35 +0800

    增加病情

--- hospital/src/main/java/com/sxxy/hospital/patient/controller/IllnessController.java
+++ hospital/src/main/java/com/sxxy/hospital/patient/controller/IllnessController.java
@@ -6,11 +6,12 @@ import com.sxxy.hospital.patient.mapper.IllnessMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 //跳转页面
 @Controller
@@ -40,6 +41,30 @@ public class IllnessController {
             return "patient/error";
         }
 
+    }
+
+    //增加实时病情
+    @PostMapping("/illnessAdd")
+    public String illnessAdd(Illness illness){
+        try {
+            illnessMapper.save(illness);
+        }catch (Exception e){
+            return "patient/error";
+        }
+        return "redirect:/illness/illnessAll";
+    }
+
+    //查询全部病情
+    @ResponseBody
+    @GetMapping("/findAllIllness")
+    public Set<String> findAllIllness(){
+        List<Illness> list = illnessMapper.findAll();
+        Set<String> set = new HashSet<>();
+        for (Illness in:list){
+           String num =  in.getIllnessNum();
+            set.add(num);
+        }
+        return set;
     }
 
 }
--- hospital/src/main/java/com/sxxy/hospital/patient/mapper/BillMapper.java
+++ hospital/src/main/java/com/sxxy/hospital/patient/mapper/BillMapper.java
@@ -10,4 +10,5 @@ import org.springframework.stereotype.Repository;
 public interface BillMapper extends JpaRepository<Bill,Object> {
 
 
+
 }
