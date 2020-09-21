e182f94b85ea20fba6b31255d87318fca92ec939
LIUXIAOBO-SHUAI\Administrator <lxb>
2020-09-21 16:22:14 +0800

    曹阳

--- hospital/src/main/java/com/sxxy/hospital/equipment/controller/EquipmentController.java
+++ hospital/src/main/java/com/sxxy/hospital/equipment/controller/EquipmentController.java
@@ -4,6 +4,8 @@ import com.sxxy.hospital.equipment.entity.Equipment;
 import com.sxxy.hospital.equipment.mapper.EquipmentMapper;
 import com.sxxy.hospital.equipment.service.EquipmentService;
 import com.sxxy.hospital.personnel.entity.Doctor;
+import org.apache.shiro.authz.annotation.Logical;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -25,6 +27,7 @@ public class EquipmentController {
 
     //查看所有器材
     @RequestMapping("/equipments")
+    @RequiresRoles(value={"admin","other","doctor"},logical = Logical.OR)
     public String allEquipment(Model model) {
         List<Equipment> equipments = new ArrayList<>();
         equipments = equipmentMapper.findAll();
@@ -34,6 +37,7 @@ public class EquipmentController {
 
     //修改器材现状
     @RequestMapping("/editEquipmentNowState")
+    @RequiresRoles(value={"admin","other"},logical = Logical.OR)
     public String editEquipmentNowState(String equipmentState,String tag) {
         String[] strs = tag.split(",");
         System.out.println(equipmentState);
@@ -49,6 +53,7 @@ public class EquipmentController {
 
     //增加器材
     @RequestMapping("/addEquipment")
+    @RequiresRoles(value={"admin","other"},logical = Logical.OR)
     public String addEquipment(String equipmentNum, String equipmentName, String equipmentState, Double equipmentPrice, String equipmentSite,Date equipmentTime) {
         Equipment addEquipment = new Equipment(equipmentNum,equipmentName,equipmentState,equipmentPrice,equipmentSite,equipmentTime);
         if (addEquipment != null) {
--- hospital/src/main/java/com/sxxy/hospital/equipment/controller/EquipmentPersonnelController.java
+++ hospital/src/main/java/com/sxxy/hospital/equipment/controller/EquipmentPersonnelController.java
@@ -1,5 +1,7 @@
 package com.sxxy.hospital.equipment.controller;
 
+import org.apache.shiro.authz.annotation.Logical;
+import org.apache.shiro.authz.annotation.RequiresRoles;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,17 +11,20 @@ public class EquipmentPersonnelController {
 
     //查看所有房间
     @RequestMapping("/allroom")
+    @RequiresRoles(value={"admin","other","doctor"},logical = Logical.OR)
     public String allRoom(){
         return "equipment/room/rooms";
     }
 
     //增加器材
     @RequestMapping("/addequipment")
+    @RequiresRoles(value={"admin","other"},logical = Logical.OR)
     public String addEquipment(){
         return "equipment/equipment/addequipments";
     }
 
     //增加药材
     @RequestMapping("/addconsumables")
+    @RequiresRoles(value={"admin","other"},logical = Logical.OR)
     public String addConsumables(){return "equipment/consumables/addconsumables";}
 }
